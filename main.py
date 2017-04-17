85969768fe4e92ef584caf6015a698064202d5cd
Kenan Deng <dknyxh@gmail.com>
2017-04-17 16:45:08 -0400

    Add font path in settings.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 
 api_key = settings.api_key
 user_name = settings.user_name
+font_path = settings.font_path
 
 resolve_name_request = requests.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={}&vanityurl={}".format(api_key, user_name))
 
@@ -20,7 +21,7 @@ for each_game in games:
 	if 'name' in each_game:
 		games_playtime[each_game['name']] = each_game['playtime_forever']
 
-wordcloud = WordCloud(font_path = '/Library/Fonts/Impact.ttf',width = 4096,height = 3072, max_words = 1000, min_font_size = 10, background_color = 'white').generate_from_frequencies(games_playtime)
+wordcloud = WordCloud(font_path = font_path,width = 4096,height = 3072, max_words = 1000, min_font_size = 10, background_color = 'white').generate_from_frequencies(games_playtime)
 wordcloud.to_file('output.png')
 # except Exception as e:
 # 	print(e)
