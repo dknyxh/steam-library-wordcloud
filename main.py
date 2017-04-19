02f52d1c5ceab59b8e35a84c99d2333e7790b11b
Kenan Deng <dknyxh@gmail.com>
2017-04-19 11:16:18 -0400

    Add a grey color function.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from wordcloud import WordCloud
 import settings
 import requests
+import random
 
 api_key = settings.api_key
 user_name = settings.user_name
 font_path = settings.font_path
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(0, 60)
 
 resolve_name_request = requests.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={}&vanityurl={}".format(api_key, user_name))
 
@@ -21,7 +26,7 @@ for each_game in games:
 	if 'name' in each_game:
 		games_playtime[each_game['name']] = each_game['playtime_forever']
 
-wordcloud = WordCloud(font_path = font_path,width = 4096,height = 3072, max_words = 1000, min_font_size = 10, background_color = 'white').generate_from_frequencies(games_playtime)
+wordcloud = WordCloud(font_path = font_path, color_func=grey_color_func, width = 4096,height = 3072, max_words = 1000, min_font_size = 10, background_color = 'white').generate_from_frequencies(games_playtime)
 wordcloud.to_file('output.png')
 # except Exception as e:
 # 	print(e)
